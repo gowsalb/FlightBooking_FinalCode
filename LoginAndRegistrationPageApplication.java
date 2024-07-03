7d22a949cc9716b36b48810b3b12a54f071b539a
gowsalb <138767449+gowsalb@users.noreply.github.com>
2024-07-03 12:31:27 +0530

    Create LoginAndRegistrationPageApplication.java

---
+++ LoginAndRegistrationPageApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint.LoginAndRegistrationPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegistrationPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAndRegistrationPageApplication.class, args);
+	}
+
+}
